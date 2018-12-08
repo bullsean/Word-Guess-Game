ea6bd99d8dae4c34ed713f687b0ef2fa28ef3a65
bullsean <41093836+bullsean@users.noreply.github.com>
2018-12-08 10:54:53 -0600

    Initial file creation with a little bit of code

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+// // Use key events to listen for the letters that your players will type.
+
+// Display the following on the page:
+
+//Message that states: Press any key to get started!
+//Listener for a key being pressed
+//Function to select random word from word bank
+//Random word is displayed as dashes based on its length (array)
+//Counter when the person wins that adds one to their score
+//Function that shows letters guessed
+//Funtion that listens for letters and displays correct ones
+//Function that listens for letters and countsdown based on guesses
+//Counter that removes one from letters remaining
+//Display guessed letters
+//After word is guessed, add one to wins
+//After guesses are gone, show the word
+//After the game is done, select new word without refresh.
+
+// Wins: (# of times user guessed the word correctly).
+
+// If the word is madonna, display it like this when the game starts: _ _ _ _ _ _ _.
+
+// As the user guesses the correct letters, reveal them: m a d o _ _ a.
+
+// Number of Guesses Remaining: (# of guesses remaining for the user).
+
+// Letters Already Guessed: (Letters the user has guessed, displayed like L Z Y H).
+
+// After the user wins/loses the game should automatically choose another word and make the user play it.
